7f378948bd3ef8775eca0766593ca80673e426b3
mixoni <jmix23@gmail.com>
2018-07-23 22:48:58 +0200

    Initial commit

---
+++ MainScript.jsx
@@ -0,0 +1,44 @@
+﻿var pos = document.getElementById('center').getBoundingClientRect(),
+    radiusSat = document.getElementById('sat1').offsetWidth * 0.5,
+    radius = document.getElementById('center').offsetWidth * 0.5,
+    cx = pos.left + radius,
+    cy = pos.top + radius,
+    
+    x, y, angle = 0, angles = [],
+    spc = 360 / 20,
+    deg2rad = Math.PI / 180,
+    i = 0;
+
+for(;i < 20; i++) {
+    angles.push(angle);
+    angle += spc;
+}
+
+/// space out radius
+radius += (radiusSat + 10);
+setInterval(function() {
+    loop();
+}, 1000);
+
+//loop();
+
+function loop() {
+
+    for(var i = 0; i < angles.length; i++) {
+        var circle = document.getElementById('sat' + i);
+        angle = angles[i];
+        //console.log(angle);
+            
+        x = cx + radius * Math.cos(angle * deg2rad);
+        y = cy + radius * Math.sin(angle * deg2rad);
+        
+        circle.style.left = (x - radiusSat).toString() + "px";
+        var left = x - radiusSat;
+        var top = y - radiusSat;
+        circle.style.top = (y - radiusSat).toString()+ "px";
+        
+        angles[i] = angles[i] + 13;
+    }
+    
+    //requestAnimationFrame(loop);
+}
